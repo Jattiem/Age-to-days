697aa224ebec2e9e45696dba0a3850103ef14ca6
jattiemyaseen <yaseenjattiem0000@gmail.com>
2022-06-20 11:53:38 +0200

    done

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ const calculate3 = document.getElementById('calculate3');
 calculate3.addEventListener('click',()=>{
     let age = document.getElementById('age').value;
     let total = (age * 365);
-    document.getElementById('display3').innerHTML = total + 'days.';
+    document.getElementById('display3').innerHTML = total + '<strong>days.</strong>';
 }
 );
+/********************************/
